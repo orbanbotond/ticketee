62befabc8225fb12aa376c8bd3904755fd2aaf64
Orban Botond <orbanbotond@gmail.com>
2013-02-24 12:40:31 +0200

    Add title functionality for show page

--- spec/integration/creation_projects_spec.rb
+++ spec/integration/creation_projects_spec.rb
@@ -8,5 +8,10 @@ feature 'Creating Projects' do
     fill_in 'Description', :with => "A text-editor for OS X"
     click_button 'Create Project'
     page.should have_content('Project has been created.')
+
+    project = Project.find_by_name("TextMate 2")
+    page.current_url.should == project_url(project)
+    title = "TextMate 2 - Projects - Ticketee"
+    find("title").should have_content(title)
   end
 end
