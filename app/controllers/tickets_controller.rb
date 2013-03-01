f464c19f18c085f6c5b8a2d10cba05c88ecb4787
Orban Botond <orbanbotond@gmail.com>
2013-03-02 00:19:40 +0200

    Restrict ticket updating to only git push those who have permission

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,6 +3,7 @@ class TicketsController < ApplicationController
   before_filter :find_project
   before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
   before_filter :authorize_create!, :only => [:new, :create]
+  before_filter :authorize_update!, :only => [:edit, :update]
 
   def update
     if @ticket.update_attributes(params[:ticket])
@@ -58,4 +59,10 @@ private
       redirect_to @project
     end
   end
+  def authorize_update!
+    if !current_user.admin? && cannot?("edit tickets".to_sym, @project)
+    flash[:alert] = "You cannot edit tickets on this project."
+    redirect_to @project
+  end
+end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -20,10 +20,25 @@ describe TicketsController do
       sign_in(:user, user)
       define_permission!(user, "view", project)
     end
+    def cannot_update_tickets!
+      response.should redirect_to(project)
+      flash[:alert].should eql("You cannot edit tickets on this project.")
+    end
     def cannot_create_tickets!
       response.should redirect_to(project)
       message = "You cannot create tickets on this project."
       flash[:alert].should eql(message)
+    end
+    it "cannot edit a ticket without permission" do
+      get :edit, { :project_id => project.id, :id => ticket.id }
+      cannot_update_tickets!
+    end
+    it "cannot update a ticket without permission" do
+      put :update, { :project_id => project.id,
+                     :id => ticket.id,
+                     :ticket => {}
+                   }
+      cannot_update_tickets!
     end
     it "cannot begin to create a ticket" do
       get :new, :project_id => project.id
